36392a1d3747bbc1f5697cc2a93a8c67a24630ae
Faisal <phesalbarwez@gmail.com>
2024-11-03 03:33:32 +0530

    switch to this after login

--- EasyD/MainApp/url.py
+++ EasyD/MainApp/url.py
@@ -5,7 +5,9 @@ from MainApp import views
 urlpatterns=[
     path("",views.home, name="home"),
     path('login/', views.login_view, name='login'),
+    path("area/",views.area_view, name='area' ),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path("home/",views.home,name="home"),
+    
 ]
