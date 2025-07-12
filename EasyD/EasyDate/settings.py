b918d7a65f8e99eb867024a402ac3f6b96111454
Faisal <phesalbarwez@gmail.com>
2025-07-12 23:13:45 +0300

    added google reviews

--- EasyD/EasyDate/settings.py
+++ EasyD/EasyDate/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+from decouple import config
+
+GOOGLE_PLACES_API_KEY = config('GOOGLE_PLACES_API_KEY', default='')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
